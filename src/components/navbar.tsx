f7a36fe48192beb63c3ce470ccd3274db748a5f2
Muhammad Taha <tahahero7890@gmail.com>
2025-01-01 14:04:24 +0500

    .

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useRef } from "react";
+import styles from './styles/navbar.module.css'
 
 function NavBar() {
 
@@ -21,19 +22,19 @@ function NavBar() {
 }
 
   return (
-    <div className="flex justify-between px-5 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-[22px] items-center" ref={Reff2} >
-      <p className="text-[20px] font-bold bg-gradient-to-br from-[#13B0F5] via-[#CA24B4] bg-clip-text text-transparent leading-[0.8]" > Taha&apos;s <br /><span className="text-[18px]">Portfolio</span></p>
-      <div className="md:flex" id="navDiv">
-        <ul className="md:flex gap-8 hidden" id="navUl" ref={Reff}>
-          <Link href="/" onClick={navClose}><li className="hover:text-[#42446E] text-[14px] lg:text-[16px]">Home</li></Link>
-          <Link href="/#skills" onClick={navClose}><li className="hover:text-[#42446E] text-[14px] lg:text-[16px]">Tech Stacks</li></Link>
-          <Link href="/#projects" onClick={navClose}><li className="hover:text-[#42446E] text-[14px] lg:text-[16px]">Projects</li></Link>
-          <Link href="/about" onClick={navClose}><li className="hover:text-[#42446E] text-[14px] lg:text-[16px]">About</li></Link>
-          <Link href="/contact" onClick={navClose}><li className="hover:text-[#42446E] text-[14px] lg:text-[16px]">Contact</li></Link>
+    <div className={styles.navbar} ref={Reff2} >
+      <p className={styles.navLogo}> Taha&apos;s <br /><span>Portfolio</span></p>
+      <div className={styles.navitems} id="navDiv">
+        <ul id="navUl" ref={Reff}>
+          <Link href="/" onClick={navClose}><li>Home</li></Link>
+          <Link href="/#skills" onClick={navClose}><li>Tech Stacks</li></Link>
+          <Link href="/#projects" onClick={navClose}><li>Projects</li></Link>
+          <Link href="/about" onClick={navClose}><li>About</li></Link>
+          <Link href="/contact" onClick={navClose}><li>Contact</li></Link>
         </ul>
       </div>
-      <div className="flex md:hidden">
-        <FontAwesomeIcon icon={faBars} className="text-[#666666] text-[24px] cursor-pointer" onClick={() => {
+      <div className={styles.hamburger}>
+        <FontAwesomeIcon icon={faBars} className={styles.hamburgerI} onClick={() => {
 
           if (Reff.current && Reff2.current) {
             if (Reff.current.style.display == '') {
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,34 +1,35 @@
 import Image from "next/image"
+import styles from './styles/skills.module.css'
 
 function Skills() {
     return (
-        <div className="px-5 sm:px-7 md:px-9 lg:px-11 xl:px-12 py-8 md:py-10 lg:py-12 xl:py-14 flex flex-col items-center gap-12 sm:gap-14 md:gap-16 lg:gap-18 xl:gap-20" id="skills">
-            <div className="text-center flex flex-col gap-[1px]">
-                <p className="text-[26px] sm:text-[28px] md:text-[30px] lg:text-[32px] xl:text-[36px] font-bold text-[#42446E]">My Tech Stack</p>
-                <p className="text-[#666666] text-[14px] sm:text-[16px] md:text-[20px] lg:text-[20px] xl:text-[24px] leading-[1.1]"> Technologies I&apos;ve been working with recently</p>
+        <div className={styles.skills} id="skills">
+            <div className={styles.skillshead}>
+                <p>My Tech Stack</p>
+                <span> Technologies I&apos;ve been working with recently</span>
             </div>
-            <div className="grid w-full self-center grid-cols-[1fr,1fr,1fr,1fr,1fr] gap-y-12 sm:gap-y-14 md:gap-y-16 lg-gap-y-18 xl:gap-y-20 gap-x-6">
-                <div className="flex justify-center">
+            <div className={styles.skillsitems}>
+                <div>
                     <Image src='/html.png' alt="html" width={105} height={105}></Image>
-                </div><div className="flex justify-center">
+                </div><div>
                     <Image src='/css.png' alt="css" width={105} height={105}></Image>
-                </div><div className="flex justify-center">
+                </div><div>
                     <Image src='/js.png' alt="js" width={105} height={105}></Image>
-                </div><div className="flex justify-center">
+                </div><div>
                     <Image src='/ts.png' alt="ts" width={105} height={105}></Image>
-                </div><div className="flex justify-center">
+                </div><div>
                     <Image src='/tailwind.png' alt="tailwind" width={105} height={105}></Image>
-                </div><div className="flex justify-center">
+                </div><div>
                     <Image src='/react.png' alt="react" width={105} height={105}></Image>
-                </div><div className="flex justify-center">
+                </div><div>
                     <Image src='/next.png' alt="nextjs" width={105} height={105}></Image>
-                </div><div className="flex justify-center">
+                </div><div>
                     <Image src='/shadecn.png' alt="shadecn" width={105} height={105}></Image>
                 </div>
-                <div className="flex justify-center">
+                <div>
                     <Image src='/vs.png' alt="vscode" width={105} height={105}></Image>
                 </div>
-                <div className="flex justify-center">
+                <div>
                     <Image src='/github.png' alt="github" width={105} height={105}></Image>
                 </div>
             </div>
